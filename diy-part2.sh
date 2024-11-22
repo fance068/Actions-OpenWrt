b2cf03aa0c04b69efec407554d964854299d1b9a
fance068 <39392319+fance068@users.noreply.github.com>
2024-11-22 19:15:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/10.10.30.1/10.8.1.99/g' package/base-files/files/bin/config_generate
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
